9a49c0b605220422c11b36b14724b573924dbd0b
on14an <112744129+On14an2@users.noreply.github.com>
2023-03-11 19:59:05 +0700

    Add files via upload

---
+++ Cat.cs
@@ -0,0 +1,76 @@
+﻿namespace tamagochi
+{
+    public struct Food
+    {
+        public int heath;
+        public string type;
+    }
+    class Cat
+    {
+        private string name;
+        private int satiety;
+
+        public Cat(string name = "Blank", int hunger = 0)
+        {
+            this.name = name;
+            this.satiety = hunger;
+        }
+        public void Meow()
+        {
+            Console.WriteLine("Мяу");
+        }
+        public void Eat(Food food)
+        {
+            Console.WriteLine($"Кот съел {food.type}. {food.type} и восстановил {food.heath} здоровья.");
+            satiety += food.heath;
+        }
+
+        public static void Info(Cat a)
+        {
+            Console.WriteLine($"{a.name}, {a.satiety}");
+        }
+        public void CheckHunger()
+        {
+            Console.WriteLine($"Здоровье: {satiety}");
+        }
+        public void Play()
+        {
+            Console.WriteLine($"{name} играет с игрушкой!");
+            satiety -= 5;
+            if (satiety < 0)
+            {
+                Console.WriteLine($"{name} Помер!");
+            }
+            else
+            {
+                Console.WriteLine($"{name} Устал");
+                CheckHunger();
+            }
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"{name} бегает за мышкой!");
+            satiety -= 10;
+            if (satiety < 0)
+            {
+                Console.WriteLine($"{name} Помер!");
+            }
+            else
+            {
+                Console.WriteLine($"{name} Устал");
+                CheckHunger();
+            }
+        }
+        public bool IsAlive()
+        {
+            bool flag = true;
+            if (satiety < 0)
+            {
+                flag = false;
+            }
+            return flag;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using tamagochi;
+
+namespace tamagochi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Имя кота:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Здоровье кота:");
+            int healh = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сколько начальной сытости получил?");
+            int heath = int.Parse(Console.ReadLine());
+            Cat mycat = new Cat(name, healh);
+            while (mycat.IsAlive())
+            {
+                Console.WriteLine("Что бы вы хотели что бы кот сделал? \n 1: Покормить кота \n 2: Помяукать \n 3:Поиграть \n 4: Информация о коте \n");
+                ConsoleKey key = Console.ReadKey().Key;
+                Console.WriteLine();
+                ConsoleKey key1;
+                switch (key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine();
+                        Console.WriteLine("Чем вы хотите накормить кота?");
+                        string type = Console.ReadLine();
+                        Food newfood = new Food { heath = heath, type = type };
+                        DoMeal(mycat, newfood);
+                        mycat.Meow();
+                        break;
+                    case ConsoleKey.D2:
+                        mycat.Meow();
+                        break;
+
+                    case ConsoleKey.D4:
+                        Cat.Info(mycat);
+                        break;
+
+                    case ConsoleKey.D3:
+                        Console.WriteLine("Как ты хочешь что бы кот играл?");
+                        Console.WriteLine("1: Играть с игрушкой \n 2: Бегать за игрушечной мышкой \n");
+                        key1 = Console.ReadKey().Key;
+                        switch (key1)
+                        {
+                            case ConsoleKey.D1:
+                                mycat.Play();
+                                break;
+                            case ConsoleKey.D2:
+                                mycat.Run();
+                                break;
+                        }
+
+                        break;
+
+                }
+            }
+            static void DoMeal(Cat c, Food f)
+            {
+                c.Eat(f);
+            }
+        }
+    }
+}
